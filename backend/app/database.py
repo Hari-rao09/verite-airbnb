5fe483594c613b13ce75bcb8708bd2b0cd3058ee
Vaibhav <vaibhavwalia9091@gmail.com>
2026-08-14 02:13:53 +0530

    Prepare backend for deployment

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,12 +1,19 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./airclone.db"
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False},
-)
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./airclone.db")
+
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False},
+    )
+else:
+    engine = create_engine(DATABASE_URL)
+
 
 SessionLocal = sessionmaker(
     autocommit=False,
