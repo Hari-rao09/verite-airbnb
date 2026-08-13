bd0b1560b03dcdfa3f0fcd52337c0f9fd9b073cd
Vaibhav <vaibhavwalia9091@gmail.com>
2026-08-14 02:46:41 +0530

    Allow production frontend in CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -31,9 +31,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-    ],
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://air-clone-eta.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
